77814a5a20083da598f147ff93e63192c0c0cdcc
simon mawole <simonmawole2011@gmail.com>
2016-03-13 16:20:43 +0300

    added some layouts

--- app/src/main/java/com/simonmawole/app/androidnanodegree/activity/MainActivity.java
+++ app/src/main/java/com/simonmawole/app/androidnanodegree/activity/MainActivity.java
@@ -1,7 +1,33 @@
 package com.simonmawole.app.androidnanodegree.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.simonmawole.app.androidnanodegree.R;
+
 /**
  * Created by simon on 3/13/16.
  */
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onClickButton(View view){
+        Button button = (Button) view;
+        showToast(button.getText().toString());
+    }
+
+    public void showToast(String message){
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/simonmawole/app/androidnanodegree/activity/PopularMoviesActivity.java
+++ app/src/main/java/com/simonmawole/app/androidnanodegree/activity/PopularMoviesActivity.java
@@ -1,7 +1,21 @@
 package com.simonmawole.app.androidnanodegree.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.simonmawole.app.androidnanodegree.R;
+
 /**
  * Created by simon on 3/13/16.
  */
-public class PopularMoviesActivity {
+public class PopularMoviesActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_movies);
+    }
 }
